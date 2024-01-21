982c11d27730bb92b654282b56451ef5bd814ecd
Azat Kulbaev <kulbaevaazat@gmail.com>
2024-01-21 23:03:26 +0600

    new feature: preventing user to send empthy inputs, and filtering by datetime

--- api/routers/messages.ts
+++ api/routers/messages.ts
@@ -3,7 +3,31 @@ import {message, messageWithoutId} from '../types';
 import fileDb from '../fileDb';
 
 const messagesRouter = Router();
-const messages: message[] = [];
+let messages: message[] = [];
+
+messagesRouter.get('/', async (req, res) => {
+  const queryDate = req.query.datetime as string;
+  messages = await fileDb.getItems();
+
+  if (!queryDate) {
+    return res.send(messages.slice(-30));
+  }
+
+  const date = new Date(queryDate);
+  if (isNaN(date.getDate())) {
+    return res.status(403).json({error: 'Invalid datetime'});
+  }
+
+  const datetimeMessages = messages.filter((message) => {
+    return new Date(message.datetime) > date;
+  });
+
+  if (datetimeMessages) {
+    res.send(datetimeMessages);
+  } else {
+    res.send([]);
+  }
+});
 
 messagesRouter.post('/', async (req, res) => {
   const message: messageWithoutId = {
@@ -12,9 +36,14 @@ messagesRouter.post('/', async (req, res) => {
     datetime: new Date().toISOString(),
   };
 
-  const newMessage = await fileDb.addItem(message);
-  res.send(newMessage);
+  if (!message.text || message.author === '') {
+    return res.status(422).send(
+      {'error': 'Author and message must be present in the request'}
+    );
+  } else {
+    const newMessage = await fileDb.addItem(message);
+    res.status(201).send(newMessage);
+  }
 });
-
 
 export default messagesRouter;
