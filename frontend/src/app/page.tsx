2c0b23edbfbf54eac29a772b635f91b9ada84469
Azat Kulbaev <kulbaevaazat@gmail.com>
2024-01-22 03:40:16 +0600

    in UI you can pick date and see messages by picked date

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@
 import {messageList, userMessage} from '../../types';
 import {useMutation, useQuery, UseQueryResult, QueryClient} from '@tanstack/react-query';
 import axiosApi from '../axiosApi';
-import React from 'react';
+import React, {useState} from 'react';
 import {CircularProgress, Grid} from '@mui/material';
 import MessageItem from '../Components/Messages/MessageItem';
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
@@ -12,12 +12,17 @@ import {DatePicker} from '@mui/x-date-pickers/DatePicker';
 import SendMessage from '../Components/SendMessage/SendMessage';
 
 const Home = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
   const {data: messages, isLoading, refetch}: UseQueryResult<messageList[], unknown> = useQuery({
     queryKey: ['message'],
     queryFn: async () => {
       try {
-        const messagesResponse = await axiosApi.get<messageList[]>('/messages');
+        const messagesResponse = await axiosApi.get<messageList[]>('/messages', {
+          params: {
+            datetime: selectedDate?.toISOString(),
+          },
+        });
         return messagesResponse.data;
       } catch (error) {
         console.log('Error: ', error);
@@ -52,7 +57,14 @@ const Home = () => {
   return (
     <Grid container direction="column" spacing={2}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker label="Filter messages by date" sx={{margin: 2, backgroundColor: 'white'}}/>
+        <DatePicker
+          label="Filter messages by date"
+          sx={{margin: 2, backgroundColor: 'white'}}
+          onChange={(date) => {
+            setSelectedDate(date);
+            refetch();
+          }}
+        />
       </LocalizationProvider>
       <Grid item container spacing={1} style={{width: 'calc(60% + 8px)', height: 700}}>
         {messageList}
